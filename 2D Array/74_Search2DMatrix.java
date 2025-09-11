71005c77c383ce259c66025cbcfbb1537680a049
lakshaybamel <lakbamel@gmail.com>
2025-09-11 21:02:14 +0530

    2D Array: Add LeetCode 74 - Search a 2D Array (StairCase)

---
+++ 2D Array/74_Search2DMatrix.java
@@ -0,0 +1,44 @@
+/*74. Search a 2D Matrix
+Solved
+Medium
+Topics- Array, Binary Search, Matrix
+You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+ 
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+
+Example 2:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-10^4 <= matrix[i][j], target <= 10^4*/
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length-1;
+        while (row < matrix.length && col >= 0) {
+            if (target == matrix[row][col]) {
+                return true;
+            } else if (target < matrix[row][col]) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+}

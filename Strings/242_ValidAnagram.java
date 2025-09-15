7d33e0d58424f24b202502c108cdfdafae3c0a2a
lakshaybamel <lakbamel@gmail.com>
2025-09-15 19:28:25 +0530

    Strings: Add LeetCode 242 - Valid Anagram

---
+++ Strings/242_ValidAnagram.java
@@ -0,0 +1,41 @@
+/*242. Valid Anagram
+Solved
+Easy
+Topics - Hash Table, String, Sorting
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+ 
+Constraints:
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.*/
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s1[i] != t1[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

7a6cd09afe28bbda521d0fec202924c9f4ea7ec0
lakshaybamel <lakbamel@gmail.com>
2025-10-02 23:20:58 +0530

    Backtracking: Add LeetCode 51 - N-Queens

---
+++ Backtracking/51_NQueens.java
@@ -0,0 +1,70 @@
+/*51. N-Queens
+Solved
+Hard
+Topics - Array, Backtracking
+
+The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+
+// Diagram for N-Queens algorithm:
+// https://assets.leetcode.com/uploads/2020/11/13/queens.jpg
+
+Example 1:
+Input: n = 4
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+
+Example 2:
+Input: n = 1
+Output: [["Q"]]
+ 
+Constraints:
+1 <= n <= 9*/
+
+class Solution {
+    private boolean isSafe(char[][] board, int row, int col) {
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 'Q') return false;
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q') return false;
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++)
+            if (board[i][j] == 'Q') return false;
+        return true;
+    }
+
+    private void nQueens(char[][] board, int row, List<List<String>> res) {
+        if (row == board.length) {
+            res.add(construct(board));
+            return;
+        }
+
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row + 1, res);
+                board[row][j] = '.';
+            }
+        }
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> path = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            path.add(new String(board[i]));
+        }
+        return path;
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            Arrays.fill(board[i], '.');
+
+        List<List<String>> res = new ArrayList<>();
+        nQueens(board, 0, res);
+        return res;
+    }
+}

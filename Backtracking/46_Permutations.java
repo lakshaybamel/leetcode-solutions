3454e1edc70fb22dd1c676a0541a8fff8f26f95e
lakshaybamel <lakbamel@gmail.com>
2025-10-13 22:08:55 +0530

    Backtracking: Add LeetCode 46 - Permutations

---
+++ Backtracking/46_Permutations.java
@@ -0,0 +1,52 @@
+/**46. Permutations
+Solved
+Medium
+Topics - Array, Backtracking
+
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+ 
+Example 1:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+Example 2:
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+
+Example 3:
+Input: nums = [1]
+Output: [[1]]
+
+
+Constraints:
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique. */
+
+
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), new boolean[nums.length], result);
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<Integer> current, boolean[] used, List<List<Integer>> result) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+
+            used[i] = true;
+            current.add(nums[i]);
+            backtrack(nums, current, used, result);
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+}

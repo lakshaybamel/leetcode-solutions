b8cb0da8520bb6c3d24630088cc29f9f609d1706
lakshaybamel <lakbamel@gmail.com>
2025-10-21 22:14:30 +0530

    LinkedList: Add LeetCode 148 - Sort List

---
+++ Linked List/148_SortList.java
@@ -0,0 +1,98 @@
+/**148. Sort List
+Solved
+Medium
+Topics - Linked List, Two Pointers, Divide and Conquer, Sorting, Merge Sort
+
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+Example 1:
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+
+Example 2:
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+
+Example 3:
+Input: head = []
+Output: []
+
+Constraints:
+The number of nodes in the list is in the range [0, 5 * 10^4].
+-10^5 <= Node.val <= 10^5
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)? */
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode mergedLL = new ListNode(-1);
+        ListNode temp = mergedLL;
+
+        while (head1 != null && head2 != null) {
+            if (head1.val <= head2.val) {
+                temp.next = head1;
+                head1 = head1.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+
+        return mergedLL.next;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = getMid(head);
+
+        ListNode rightHead = mid.next;
+        mid.next = null;
+
+        ListNode newLeft = sortList(head);
+        ListNode newRight = sortList(rightHead);
+
+        return merge(newLeft, newRight);
+    }
+}

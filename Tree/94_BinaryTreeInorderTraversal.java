3ac6c91f9ad9a00151eacab68675aff56d4f0d6e
lakshaybamel <lakbamel@gmail.com>
2026-01-10 19:06:35 +0530

    Tree: Add LeetCode 94 - Binary Tree Inorder Traversal

---
+++ Tree/94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,61 @@
+/**94. Binary Tree Inorder Traversal
+Solved
+Easy
+Topics - Tree, Depth-First Search, Binary Tree
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,2,6,5,7,1,3,9,8]
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100 */
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+       List<Integer> ans = new ArrayList<>();
+       inorder(root, ans);
+       return ans; 
+    }
+
+    public void inorder(TreeNode root, List<Integer> ans) {
+        if(root == null) {
+            return;
+        }
+        inorder(root.left, ans);
+        ans.add(root.val);
+        inorder(root.right, ans);
+    }
+}

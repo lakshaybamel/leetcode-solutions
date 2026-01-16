97038a7202488bba2fa7a98f8e2b647a372ddf99
lakshaybamel <lakbamel@gmail.com>
2026-01-17 00:03:48 +0530

    Hashing: Add LeetCode 169 - Majority Element

---
+++ Hashing/169_MajorityElement.java
@@ -0,0 +1,51 @@
+/*169. Majority Element
+Solved
+Easy
+Topics - Array, Hash Table, Divide and Conquer, Sorting, Counting
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+Constraints:
+n == nums.length
+1 <= n <= 5 * 10^4
+-10^9 <= nums[i] <= 10^9*/
+
+
+
+
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(nums[i])) {
+                hm.put(nums[i], hm.get(nums[i]) + 1);
+            } else {
+                hm.put(nums[i], 1);
+            }
+        }
+
+        int ans = 0;
+
+        for (int key : hm.keySet()) {
+            if (hm.get(key) > nums.length / 2) {
+                ans = key;
+            }
+        }
+
+        return ans;
+    }
+}
+

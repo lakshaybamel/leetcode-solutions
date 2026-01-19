f942d464722e9c9aadd146e1ecec6273fa97541e
lakshaybamel <lakbamel@gmail.com>
2026-01-19 20:59:11 +0530

    Hashing: Add LeetCode 242 - Valid Anagram

---
+++ Hashing/242_ValidAnagram.java
@@ -0,0 +1,53 @@
+/**242. Valid Anagram
+Solved
+Easy
+Topics - Hash Table, String, Sorting
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+Constraints:
+1 <= s.length, t.length <= 5 * 10^4
+s and t consist of lowercase English letters.
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?  */
+
+
+
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+
+            if (map.get(ch) == null) {
+                return false;
+            }
+
+            if (map.get(ch) == 1) {
+                map.remove(ch);
+            } else {
+                map.put(ch, map.get(ch) - 1);
+            }
+        }
+
+        return map.isEmpty();
+    }
+}

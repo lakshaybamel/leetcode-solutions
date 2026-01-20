3848f22196fb3ca401f31ffa446f64d1ed02e2aa
lakshaybamel <lakbamel@gmail.com>
2026-01-20 21:42:43 +0530

    String: Add LeetCode 125 - Valid Palindrome

---
+++ Strings/125_ValidPalindrome.java
@@ -0,0 +1,56 @@
+/**125. Valid Palindrome
+Solved
+Easy
+Topics - Two Pointers, String
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+ 
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+Constraints:
+1 <= s.length <= 2 * 10^5
+s consists only of printable ASCII characters. */
+
+
+
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                str.append(Character.toLowerCase(ch));
+            }
+        }
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}

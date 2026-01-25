26a8049fdbb63d43397c3e7b791dba67651c4f62
lakshaybamel <lakbamel@gmail.com>
2026-01-25 18:36:08 +0530

    DP: Add LeetCode 139 - Word Break

---
+++ Dynamic Programming/139_WordBreak.java
@@ -0,0 +1,59 @@
+/**139. Word Break
+Solved
+Medium
+Topics - Array, Hash Table, String, Dynamic Programming, Trie, Memoization
+
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+Example 1:
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+ 
+Constraints:
+1 <= s.length <= 300
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique. */
+
+
+
+
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        Boolean[] memo = new Boolean[s.length()];
+        return helper(s, 0, set, memo);
+    }
+
+    private boolean helper(String s, int start, Set<String> dict, Boolean[] memo) {
+        if (start == s.length()) return true;
+
+        if (memo[start] != null) return memo[start];
+
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (dict.contains(s.substring(start, end)) &&
+                helper(s, end, dict, memo)) {
+                memo[start] = true;
+                return true;
+            }
+        }
+
+        memo[start] = false;
+        return false;
+    }
+}

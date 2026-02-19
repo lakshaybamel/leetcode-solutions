6ab298ea26e5eeaec08545ed2558d91d8a2f0d20
lakshaybamel <lakbamel@gmail.com>
2026-02-19 23:08:46 +0530

    Arrays: Add LeetCode 448 - Find All Numbers Disappeared in an Array

---
+++ Arrays/448_FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,49 @@
+/**448. Find All Numbers Disappeared in an Array
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+1 <= nums[i] <= n */
+
+
+
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        boolean[] present = new boolean[n + 1];
+
+        for (int num : nums) {
+            present[num] = true;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (!present[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}

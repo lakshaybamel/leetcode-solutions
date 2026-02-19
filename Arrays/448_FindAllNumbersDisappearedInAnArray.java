e41147598d4bb19260d8df2431b3f5326bde593f
lakshaybamel <lakbamel@gmail.com>
2026-02-19 23:11:28 +0530

    Arrays: Fix question 448 - Find All Numbers Disappeared in an Array

--- Arrays/448_FindAllNumbersDisappearedInAnArray.java
+++ Arrays/448_FindAllNumbersDisappearedInAnArray.java
@@ -1,26 +1,19 @@
 /**448. Find All Numbers Disappeared in an Array
 Solved
 Easy
-Topics
-premium lock icon
-Companies
-Hint
+Topics - Array, Hash Table
+
 Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
 
- 
-
 Example 1:
-
 Input: nums = [4,3,2,7,8,2,3,1]
 Output: [5,6]
-Example 2:
 
+Example 2:
 Input: nums = [1,1]
 Output: [2]
- 
 
 Constraints:
-
 n == nums.length
 1 <= n <= 105
 1 <= nums[i] <= n */
